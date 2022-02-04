61e64292f35f9b966387e4fb2604b82d5b34263c
TheEvilSkeleton <theevilskeleton@riseup.net>
2022-02-03 23:03:51 -0500

    Add WM support
    Co-authored-by: tinywrkb <tinywrkb@gmail.com>

--- LogisimEvolution.sh
+++ LogisimEvolution.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+case "${XDG_CURRENT_DESKTOP}:" in
+  :|wlroots:*|sway:*|Wayfire:*|river:*)
+  export "_JAVA_AWT_WM_NONREPARENTING=1"
+  ;;
+esac
+
 java -jar /app/bin/LogisimEvolution.jar  $@
